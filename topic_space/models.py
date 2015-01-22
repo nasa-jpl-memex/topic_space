0d2a2d4f0eb52a0b255d94bcd71b0a13a4249b83
Christine Doig <christine.doig.cardet@gmail.com>
2015-01-21 19:54:09 -0600

    refactor models, create lda interface

--- topic_space/models.py
+++ topic_space/models.py
@@ -1,83 +1,54 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
 
-import string
+import gensim
 
-from .utils import read_file_msr_data, stopwords_set
-
-import logging
-from gensim import corpora, models, similarities
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-MSR_DATA_FILE = "material_science/data/msr-data"
+import pandas as pd
 
 
-# Dictionaries
-# We can have different dictionaries:
-# 1 - All words in the corpus minus stop list and words that appear only once
+class LDA(object):
+    """
+    A high interface for an LDA (Latent Dirichlet Allocation) model.
 
-# STOPWORDS LIST
-# https://code.google.com/p/stop-words/
+    Parameters
+    ----------
+    corpus_file: string
+        Location of the corpus serialized in Matrix Market format
+    dict_file: string
+        Location of the dictionary
+    >>> my_lda = LDA("my_corpus.mm", "my_dict.dict")
+    """
 
-STOPWORDS_FILE = "material_science/data/stop-words_english_6_en.txt"
+    def __init__(self, corpus_file, dict_file):
+        self.corpus_file = gensim.corpora.MmCorpus(corpus_file)
+        self.dictionary = gensim.corpora.Dictionary.load(dict_file)
+        self.model = gensim.models.LdaModel(self.corpus_file, num_topics=10, id2word=self.dictionary, passes=4)
 
-DICTIONARY = 'material_science/output/msr_corpus.dict'
+    def get_top_words(self, topn=15):
+        top_words = [ self.model.show_topic(topicno, topn) for topicno in range(self.model.num_topics) ]
+        return top_words
 
-def create_dictionary(input_data, stopwords_file, output_file):
-    msr_data = read_file_msr_data(input_data)
-    stopwords = stopwords_set(stopwords_file)
-    documents = msr_data.abstract
+    def generate_termite_input(self, filename="termite.csv"):
+        """
+        Generate the csv file input for the termite plot.
 
-    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-    texts = [[word.lower() for word in document.translate(remove_punctuation_map).split() if word not in stopwords]
-                 for document in documents]
-
-    all_tokens = sum(texts, [])
-    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-    word_list = [[word for word in text if word not in tokens_once]
-             for text in texts]
-
-    dictionary = corpora.Dictionary(word_list)
-    dictionary.save(output_file) # store the dictionary, for future reference
-    print(dictionary)
-    return dictionary
-
-
-dictionary_1 = create_dictionary(MSR_DATA_FILE, STOPWORDS_FILE, DICTIONARY)
-
-#Dictionary(175991 unique tokens: [u'llactic', u'005\xba', u'y004\u2013006', u'27\xb0', u'mdbt']...)
-
-# 2 - Dictionary of material science terms
-
-# 3 - Dictionary of chemical terms
-
-
-# Different models
-
-# - Term frequency
-class MSAbstractsCorpus(object):
-    def __init__(self, input_data, stopwords_file, dictionary):
-        self.input_data = input_data
-        self.stopwords_file = stopwords_file
-        self.dictionary = dictionary
-
-    def __iter__(self):
-        dictio = corpora.Dictionary.load(self.dictionary)
-        documents = read_file_msr_data(self.input_data).abstract
-        for document in documents:
-            # assume there's one document per line, tokens separated by whitespace
-            yield dictio.doc2bow(document.lower().split())
-
-
-corpus = MSAbstractsCorpus(MSR_DATA_FILE, STOPWORDS_FILE, DICTIONARY)
-
-#for vector in corpus:
-#    print(vector)
-
-# - TF-IDF
-
-# - LSA/LSI
-
-# - LDA
-
-# - HDP
+        Parameters
+        ----------
+        filename: string
+            Desired name for the generated csv file
+        >>> my_lda = LDA("my_corpus.mm", "my_dict.dict")
+        >>> my_lda.generate_termite_input()
+        """
+        top_words = [ self.model.show_topic(topicno, topn=15) for topicno in range(self.model.num_topics) ]
+        count = 1
+        for topic in top_words:
+            if count == 1:
+                df_temp = pd.DataFrame(topic, columns=['weight', 'word'])
+                df_temp['topic'] = pd.Series(count, index=df_temp.index)
+                df = df_temp
+            else:
+                df_temp = pd.DataFrame(topic, columns=['weight', 'word'])
+                df_temp['topic'] = pd.Series(count, index=df_temp.index)
+                df = df.append(df_temp, ignore_index=True)
+            count += 1
+        df.to_csv(filename, index=False)
+        return df
