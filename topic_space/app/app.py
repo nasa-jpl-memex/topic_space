77ee92a3ed1ed763095e69e384e0c66d573f4e31
Andy R. Terrel <andy.terrel@gmail.com>
2015-03-22 16:30:14 -0500

    Remove unused histogram

--- topic_space/app/app.py
+++ topic_space/app/app.py
@@ -9,7 +9,6 @@ from bokeh.models import HoverTool
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.resources import INLINE
 from bokeh.templates import RESOURCES
-from bokeh.utils import encode_utf8
 from wordcloud import WordCloud
 
 from topic_space.wordcloud_generator import FONT_PATH, get_docs_by_year, read_file, read_sample
@@ -152,59 +151,6 @@ def get_wordcloud(req_id, interval_id):
     wordcloud.to_image().save(img_io, 'JPEG', quality=70)
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
-
-
-colors = {
-    'Black': '#000000',
-    'Red':   '#FF0000',
-    'Green': '#00FF00',
-    'Blue':  '#0000FF',
-}
-
-
-def getitem(obj, item, default):
-    if item not in obj:
-        return default
-    else:
-        return obj[item]
-
-
-
-@app.route('/topic_space/histogram/', methods=["GET", "POST"])
-def histogram():
-    # Grab the inputs arguments from the URL
-    # This is automated by the button
-    args = request.args
-
-    # Get all the form arguments in the url with defaults
-    color = colors[getitem(args, 'color', 'Black')]
-    _from = int(getitem(args, '_from', 0))
-    to = int(getitem(args, 'to', 10))
-
-    count_df = DF[['year', 'abstract']].groupby('year').count()
-    count_df = count_df.drop("January 2000")
-    fig = figure(title="Histogram of Docs Per Year")
-    fig.line(map(int, count_df.index), list(count_df['abstract']), color=color, line_width=2)
-
-    # Configure resources to include BokehJS inline in the document.
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#module-bokeh.resources
-    plot_resources = RESOURCES.render(
-        js_raw=INLINE.js_raw,
-        css_raw=INLINE.css_raw,
-        js_files=INLINE.js_files,
-        css_files=INLINE.css_files,
-    )
-
-# For more details see:
-#   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
-    script, div = components(fig, INLINE)
-    html = render_template(
-        'histogram.html',
-        plot_script=script, plot_div=div, plot_resources=plot_resources,
-        color=color, _from=_from, to=to
-    )
-    return encode_utf8(html)
 
 
 if __name__ == '__main__':
